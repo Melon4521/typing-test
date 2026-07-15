f723dd0948c2633fbf0519e0d1547ed56dffde43
melon4521 <vlad123kyz@gmail.com>
2026-07-15 13:20:32 +0300

    feat: module render/dom.ts

---
+++ src/render/dom.ts
@@ -0,0 +1,31 @@
+export interface Elements {
+  textPlaceholder: HTMLElement;
+  typingInput: HTMLInputElement;
+  visualText: HTMLElement;
+  passiveText: HTMLElement;
+  typingCaret: HTMLElement;
+
+  settingsPanel: HTMLFormElement;
+  settingsLang: HTMLElement;
+  settingsMode: HTMLElement;
+  settingsValue: HTMLElement;
+
+  btnRepeat: HTMLButtonElement;
+}
+
+export function getElements(): Elements {
+  return {
+    textPlaceholder: document.querySelector('.text__placeholder')!,
+    typingInput: document.querySelector('#typing-input')!,
+    visualText: document.querySelector('#visual-text')!,
+    passiveText: document.querySelector('#passive-text')!,
+    typingCaret: document.querySelector('#typing-caret')!,
+
+    settingsPanel: document.querySelector('#settings-panel')!,
+    settingsLang: document.querySelector('#settings-lang')!,
+    settingsMode: document.querySelector('#settings-mode')!,
+    settingsValue: document.querySelector('#settings-value')!,
+
+    btnRepeat: document.querySelector('#btn-repeat')!,
+  };
+}
