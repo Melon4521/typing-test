71948a615c1db7b31621e105faa79af584be03f3
melon4521 <vlad123kyz@gmail.com>
2026-08-01 16:48:06 +0300

    feat(render): add newTest function

--- src/render/render.ts
+++ src/render/render.ts
@@ -3,17 +3,271 @@ import type { TextsCfg } from '../text/textsConfig';
 import type { Elements } from './dom';
 
 /**
- * Renders DOM for an actual test state
+/**
+ * Sets DOM initial state and attaches listeners to the typingInput element with test logic.
  *
  * @param els - The DOM elements used by the typing test.
  * @param textsCfg - The configuration of texts.
  */
-export function renderTest(els: Elements, textsCfg: TextsCfg) {
+export function newTest(els: Elements, textsCfg: TextsCfg) {
   const text = generateText(textsCfg);
+  const state = newTestState(text, settingsStore.getMode());
+
+  els.typingInput.focus();
+  els.typingInput.value = '';
+  els.typingInput.onblur = null;
+
+  els.visualText.innerHTML = '';
+
+  // Attach a click listener to abort test
+  els.btnRepeat.onclick = () => {
+    console.log('Прерывание теста...');
+
+    if (state.abortTimeoutId !== null) {
+      clearTimeout(state.abortTimeoutId);
+    }
+
+    abortTest(state, els, textsCfg);
+  };
+
+  // Move typingCaret into the textBody element
+  els.textBody.append(els.typingCaret);
+
+  // Show the typingCaret
+  if (els.typingCaret.classList.contains('_hidden')) {
+    els.typingCaret.classList.remove('_hidden');
+  }
 
   initPassiveText(els, text);
 
-  els.typingInput.focus();
+  // Pasting in input is forbidden
+  els.typingInput.onpaste = e => {
+    e.preventDefault();
+  };
+
+  // Extra actions on key pressed before input event handler
+  els.typingInput.onkeydown = e => {
+    // If user wants to type another char (of length 1) in the end of the word and limit is already reached
+    if (
+      state.incorrectTypedCharsInEndCount > 5 &&
+      e.key.length === 1 &&
+      e.key !== ' '
+    ) {
+      e.preventDefault();
+    }
+
+    // Arrow/Delete keys were pressed - ignore them
+    if (
+      ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Delete'].includes(
+        e.code,
+      )
+    ) {
+      e.preventDefault();
+    }
+
+    // Move to the previous word, that was incorrect typed
+    if (
+      e.code === 'Backspace' &&
+      !e.ctrlKey &&
+      els.typingInput.value.length === 0 &&
+      state.currentWordIndex !== 0 &&
+      state.lastIncorrectWord !== null
+    ) {
+      e.preventDefault();
+
+      els.typingInput.value = state.lastIncorrectWord.value;
+      state.newInputLength = state.prevInputLength =
+        els.typingInput.value.length;
+      state.currentWordIndex = state.lastIncorrectWord.wordIndex;
+      state.currentCharIndex = state.lastIncorrectWord.charIndex;
+      state.activeWord = state.words[state.currentWordIndex];
+      state.incorrectTypedCharsInEndCount =
+        state.lastIncorrectWord.incorrectTypedCharsInEndCount;
+      state.typedWords.pop();
+      state.startTime = Date.now();
+      state.lastIncorrectWord = null;
+
+      renderTest(els, state);
+    }
+
+    // Ctrl + Backspace - fully erase typed word
+    if (
+      e.code === 'Backspace' &&
+      e.ctrlKey &&
+      els.typingInput.value.length != 0
+    ) {
+      e.preventDefault();
+
+      els.typingInput.value = '';
+      state.currentCharIndex = state.prevInputLength = state.newInputLength = 0;
+
+      // Get key of current word for state.statistic
+      const statisticKey =
+        state.currentWordIndex === 0
+          ? state.words[0]
+          : state.typedWords.join(' ') +
+            ' ' +
+            state.words[state.currentWordIndex];
+      const wordStatistic = state.statistic[statisticKey];
+
+      wordStatistic.corrects.length = 0;
+      wordStatistic.incorrects.length = 0;
+      wordStatistic.incorrectTypedCharsInEnd = '';
+      wordStatistic.currentlyTyped = '';
+
+      for (const [key, value] of Object.entries(wordStatistic.chars)) {
+        if (typeof value === 'number') {
+          delete wordStatistic.chars[key];
+        }
+      }
+
+      state.startTime = Date.now();
+      state.incorrectTypedCharsInEndCount = 0;
+
+      renderTest(els, state);
+    }
+  };
+
+  // Start the test when user input in typingInput
+  els.typingInput.oninput = function (e) {
+    // Interrupt the test when typingInput has been out of focus for more than 5 seconds
+    // !!! Restore in production
+    /* if (els.typingInput.onblur == null) {
+      els.typingInput.onblur = () => {
+        console.log('Прерывание теста через 5 секунд.');
+        testState.abortTimeoutId = setTimeout(() => {
+          els.typingInput.onfocus = null;
+          abortTest(testState, els, textsCfg, mode);
+        }, 5000);
+
+        els.typingInput.onfocus = () => {
+          if (testState.abortTimeoutId !== null) {
+            console.log('Отмена прерывания.');
+            clearTimeout(testState.abortTimeoutId);
+            testState.abortTimeoutId = null;
+          }
+        };
+      };
+    } */
+
+    // Get key of current word for state.statistic
+    const statisticKey =
+      state.currentWordIndex === 0
+        ? state.words[0]
+        : state.typedWords.join(' ') +
+          ' ' +
+          state.words[state.currentWordIndex];
+
+    // If it is start of the test
+    if (state.currentWordIndex === 0 && state.currentCharIndex === 0) {
+      // Hide the settingsPanel
+      if (!els.settingsPanel.classList.contains('_hidden')) {
+        settingsPanelHide(els.settingsPanel, true);
+      }
+
+      // Show the repeat button
+      if (els.btnRepeat.classList.contains('_hidden')) {
+        els.btnRepeat.classList.remove('_hidden');
+      }
+
+      // Activate the typingCaret
+      if (!els.typingCaret.classList.contains('_active')) {
+        els.typingCaret.classList.add('_active');
+      }
+
+      // Set finish timeout in time mode when test starts
+      if (state.mode === 'time') {
+        const finishMilliseconds = settingsStore.getValue(state.mode) * 1000;
+
+        // Remove listeners from typingInput, shows settingsPanel and calculate stats with finishTest function
+        state.finishTimeoutId = setTimeout(() => {
+          els.typingInput.oninput = null;
+          els.typingInput.onkeydown = null;
+
+          settingsPanelHide(els.settingsPanel, false);
+
+          state.endTime = Date.now();
+
+          if (state.lastIncorrectWord !== null) {
+            const prevWordStatistic =
+              state.statistic[state.typedWords.join(' ')];
+
+            for (const key of prevWordStatistic.incorrects) {
+              prevWordStatistic.chars[key] =
+                state.endTime - Number(prevWordStatistic.chars[key]);
+            }
+          }
+
+          if (state.activeWord !== null) {
+            // Get key of current word for state.statistic
+            const statisticKey =
+              state.currentWordIndex === 0
+                ? state.words[0]
+                : state.typedWords.join(' ') +
+                  ' ' +
+                  state.words[state.currentWordIndex];
+            const wordStatistic = state.statistic[statisticKey];
+
+            if (wordStatistic.incorrects.length !== 0) {
+              for (const key of wordStatistic.incorrects) {
+                wordStatistic.chars[key] =
+                  state.endTime - Number(wordStatistic.chars[key]);
+              }
+            }
+          }
+
+          const testStatistics = finishTest(state);
+
+          renderTestStatistics(testStatistics);
+        }, finishMilliseconds);
+      }
+    }
+
+    if (state.activeWord === null) {
+      state.currentCharIndex = 0;
+      state.prevInputLength = 0;
+      state.newInputLength = 0;
+      state.activeWord = state.words[state.currentWordIndex];
+
+      state.statistic[statisticKey] = {
+        currentlyTyped: '',
+        corrects: [],
+        incorrects: [],
+        chars: {},
+        incorrectTypedCharsInEnd: '',
+      };
+
+      if (state.currentWordIndex === 0) {
+        state.startTime = Date.now();
+      }
+    }
+
+    state.newInputLength = els.typingInput.value.length;
+
+    // Length increased - a char was typed
+    if (state.newInputLength - state.prevInputLength > 0 && e.data !== null) {
+      const { shoudCleanInput, testStatistics } = typeChar(state, e.data);
+
+      if (shoudCleanInput) {
+        els.typingInput.value = '';
+      }
+
+      if (testStatistics !== null) {
+        els.typingInput.oninput = null;
+        els.typingInput.onkeydown = null;
+
+        settingsPanelHide(els.settingsPanel, false);
+
+        renderTestStatistics(testStatistics);
+      }
+    } else {
+      // Length decreased - a character was deleted
+      deleteChar(state);
+    }
+
+    renderTest(els, state);
+  };
+}
 }
 
 /**
